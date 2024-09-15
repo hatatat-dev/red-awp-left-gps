f3dcce38960eb4e3b06729d5bd3ca86af4a63e76
hatatat-dev <hatatat@glowstonemania.com>
2024-09-15 13:17:39 -0700

    [gps] autonomous function using GPS

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# red-awp-left 
-21919A VEX high stakes red autonomous win point on left side
+# red-awp-left-gps
+21919A VEX high stakes red autonomous win point on left side with GPS
--- src/main.py
+++ src/main.py
@@ -3,30 +3,32 @@
 from lib.log import *
 from high_stakes.events import *
 
-open_log("red-awp-left.csv")
+open_log("red-awp-left-gps.csv")
+
 
 def driver_function():
     pass
 
-def autonomous_function():
+
+def original_autonomous_function():
     intake_1st_stage.set_velocity(470, RPM)
     intake_2nd_stage.set_velocity(470, RPM)
     pid_driver.drive(-1000, True)
     clamp.set(True)
     pid_turner.turn(80, FRAME_HEADING_RELATIVE)
-    
+
     reset_odometry()
-    
+
     intake_1st_stage.spin(REVERSE)
     intake_2nd_stage.spin(FORWARD)
     wait(1000, MSEC)
     pid_driver.drive(550)
     pid_turner.turn(80, FRAME_HEADING_RELATIVE)
     pid_driver.drive(270)
-    
+
     wait(1000, MSEC)
     reset_odometry()
-    
+
     pid_driver.drive(-150)
     pid_turner.turn(20, FRAME_HEADING_RELATIVE)
     pid_driver.drive(170)
@@ -49,6 +51,25 @@ def autonomous_function():
     # pid_turner.turn(75, FRAME_HEADING_RELATIVE)
     # pid_driver.drive(-400)
 
+
+def autonomous_function():
+    intake_1st_stage.set_velocity(470, RPM)
+    intake_2nd_stage.set_velocity(470, RPM)
+
+    pid_mover.move(Position(1200, 600), FRAME_ABSOLUTE)
+
+    wait(1000, MSEC)
+    reset_odometry()
+
+    pid_turner.turn(0, FRAME_ABSOLUTE)
+
+    wait(1000, MSEC)
+    reset_odometry()
+
+    pid_mover.move(Position(0, 1200), FRAME_ABSOLUTE)
+
+    wait(1000, MSEC)
+    reset_odometry()
 
 
 init_event_handling()
