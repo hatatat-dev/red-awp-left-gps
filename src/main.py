c039ae817f2492ef66ce1e170f1c827142484eb4
28adityab <aditya.banwasi@gmail.com>
2024-09-15 12:22:09 -0700

    csv

--- src/main.py
+++ src/main.py
@@ -9,26 +9,26 @@ def driver_function():
     pass
 
 def autonomous_function():
-    intake_1st_stage.set_velocity(500, RPM)
-    intake_2nd_stage.set_velocity(500, RPM)
+    intake_1st_stage.set_velocity(470, RPM)
+    intake_2nd_stage.set_velocity(470, RPM)
     pid_driver.drive(-1000, True)
     clamp.set(True)
     pid_turner.turn(80, FRAME_HEADING_RELATIVE)
     
-    wait(1000, MSEC)
     reset_odometry()
     
     intake_1st_stage.spin(REVERSE)
     intake_2nd_stage.spin(FORWARD)
+    wait(1000, MSEC)
     pid_driver.drive(600, True)
-    pid_turner.turn(90, FRAME_HEADING_RELATIVE)
+    pid_turner.turn(80, FRAME_HEADING_RELATIVE)
     pid_driver.drive(270, True)
     
     wait(1000, MSEC)
     reset_odometry()
     
     pid_driver.drive(-150, True)
-    pid_turner.turn(15, FRAME_HEADING_RELATIVE)
+    pid_turner.turn(20, FRAME_HEADING_RELATIVE)
     pid_driver.drive(160, True)
 
     wait(1000, MSEC)
